83023836dd1ec9dd8510d3fe38d4b0582e6fcd41
Chuong <hoangchuongitutc@gmail.com>
2024-03-30 15:43:08 +0700

    refactor(dev-env): update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const logger = new Logger(bootstrap.name);
-	const app = await NestFactory.create(AppModule);
-	const config_service = app.get(ConfigService);
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-		}),
-	);
-	await app.listen(config_service.get('PORT'), () =>
-		logger.log(`Application running on port ${config_service.get('PORT')}`),
-	);
+  const logger = new Logger(bootstrap.name);
+  const app = await NestFactory.create(AppModule);
+  const config_service = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(config_service.get('PORT'), () =>
+    logger.log(`Application running on port ${config_service.get('PORT')}`),
+  );
 }
 bootstrap();
